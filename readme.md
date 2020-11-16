ae6b0b79da92f069b02b09ad4547715996f9202a
Gagan Verma <46402550+zatch3301@users.noreply.github.com>
2020-11-16 21:22:17 +0530

    Add files via upload

---
+++ readme.md
@@ -0,0 +1,18 @@
+## Visualization webapp in R using shiny
+
+## Blood Bank Visualization:
+This is a simple shiny app which will make it easier to locate blood banks in India along with pin-code, addresses, website and number.
+
+## Data:
+Data for the blood bank application is sourced from [Open Government Data Platform](https://data.gov.in/). The original data is available as a csv file called "Blood Bank Directory (updated Till Last Month)".
+
+
+## Data limitation:
+One of the limitations of this application is that the data is not accurate. The data for few bloood bank are missing and NA values.
+
+Hence i have replace the NA values with blank and taken only those columns which have sufficient data available.
+
+## Help:
+In case you observe the data projected on map is incorrect and you have more accurate or additional information that could be updated it will be great if you can email me and i will update the same.
+
+## Thank you
---
+++ zatch_app.R
@@ -0,0 +1,100 @@
+library(shiny)
+library(leaflet)
+library(tidyverse)
+library(janitor)
+library(plotly)
+library( htmltools)
+library(DT)
+library(dplyr)
+library(formattable)
+
+ui <- navbarPage("Blood Bank", id="mainnavbar",
+                 tabPanel("Map", leafletOutput("bbmap", width = "100%", height = 800)),
+                 tabPanel("Analysis", fluidPage(
+                   titlePanel("Data Analysis"),
+                   plotlyOutput("plot1", height = "700px")
+                  )
+                 ),
+                 tabPanel("Data", DT::dataTableOutput("mytable")),
+                 tabPanel("Read Me",includeMarkdown("readme.md"))
+  
+  )
+
+server <- function(input, output, session) {
+ read_csv("blood-banks.csv", 
+           col_types = cols(Latitude = col_double(), 
+                            Longitude = col_double())) %>% 
+    select(c(-1)) %>% 
+    clean_names() %>%
+    filter(latitude != '#N/A',
+    longitude != '#N/A') -> bb_data
+  
+  # bb_data$category %>% unique()
+  pal <- colorFactor(pal = c("#1b9e77", "#d95f02", "#7570b3"), 
+                     domain = c("Charity", "Government", "Private"))
+    
+  output$bbmap <- renderLeaflet({
+    bb_data %>% 
+      leaflet() %>% 
+      addTiles() %>% 
+      addCircleMarkers(~longitude, ~latitude,
+                 labelOptions = labelOptions(noHide = F,
+                                             offset = c(0,-45), 
+                                             direction = "auto"),
+                 clusterOptions = markerClusterOptions(),
+                 color = ~pal(bb_data$category),
+                 popup =  paste0("<b>Name : </b>",as.raw(bb_data$blood_bank_name),"<br>",
+                                 "<b>District : </b>",as.character(bb_data$district),"<br>",
+                                 "<b>City : </b>",as.character(bb_data$city),"<br>",
+                                 "<b>State : </b>",as.character(bb_data$state),"<br>",
+                                 "<b>Pincode : </b>",as.integer(bb_data$pincode),"<br>",
+                                 "<b>Contact : </b>",as.character(bb_data$contact_no),"<br>",
+                                 "<b>Website : </b>",as.character(bb_data$website))) %>% 
+      addLegend(position = "bottomright",
+                pal = pal, 
+                values = bb_data$category, 
+                na.label = "Not Available" )
+  })
+  
+  
+  
+  output$mytable <- DT::renderDataTable({
+    bb_data %>% 
+      clean_names() %>% 
+      select(c(1:26)) -> dd
+    datatable(dd, rownames = FALSE, colnames = c('Blood Bank Name' =1))
+  })
+  
+  bb_data %>% 
+    mutate(state = state %>% factor() %>% fct_infreq()) %>% 
+    mutate(district = district %>% factor() %>% fct_infreq()) %>% 
+    mutate(city = city %>% factor() %>% fct_infreq()) %>% 
+    select(c(2:4))-> df
+  
+  output$plot1 <- renderPlotly({
+    
+    ggplotly(
+      ggplot(df,aes(state))+
+        geom_bar(aes(fill=state))+
+        coord_flip()+
+        labs(x = "State",
+             y = "No.of Blood Bank",
+             title = "Blood Banks",
+             caption = "By gagan")+
+        theme_minimal()+
+        theme(axis.text.x = element_text(size = 10,
+                                         face = "bold",
+                                         angle = 0),
+              axis.text.y = element_text(size = 10,
+                                         face = "bold"),
+              axis.title = element_text(size = 18,
+                                        face = "bold"),
+              legend.position = "none")
+    )     
+
+  })
+
+  
+}
+
+shinyApp(ui, server)
